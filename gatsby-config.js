b670b61098e7de0a99fc2672330d96366970cce9
Dawn <dawn.wages@gmail.com>
2020-12-07 18:27:32 -0500

    ATR-0 gatsby config for gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,15 @@ module.exports = {
         icon: `src/images/LogoAtTheRoot.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingId: [
+          "G-0HQ1N6YQ15", // Google Analytics / GA
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
